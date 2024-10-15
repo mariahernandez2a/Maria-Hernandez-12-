275c2146b7c266bb213fa63b8fdd33570f34dd81
mariahernandez2a <mjhf17340@gmail.com>
2024-10-15 07:53:22 -0600

    script.js

--- TRABAJOS/dado/script.js
+++ TRABAJOS/dado/script.js
@@ -1,7 +1,7 @@
 function dado(){
     const min = +document.querySelector('#numMin').value;
     const max = +document.querySelector('#numMax').value;
-    let random = Math.round(Math.random()*(max-min+1)+min)
+    let random = Math.round(Math.random()*(max-min)+min)
     const salida = document.querySelector('#out');
     salida.textContent = random;
 }
